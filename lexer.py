3ee204cffe23be0095173379404cbb4cc2312a5b
jiading <jiading@jiading.com>
2015-01-13 17:58:49 -0600

    Need to fix the infinite loop on lexer.py

--- lexer.py
+++ lexer.py
@@ -14,9 +14,20 @@ class TokType(Enum):
 
     # Not sure about these
     THROWAWAY = -1
+    WHITESPACE = 0
     NEWLINE = 1
     COMMENT = 3
+    LPAREN = '('
+    RPAREN = ')'
+    LBRACKET = '{'
+    RBRACKET = '}'
+    AT = '@'
+    SEMICOLON = ':'
+    COLON = ';'
     MULT = '*'
+    PLUS = '+'
+    COMMA = ','
+    DOT = '.'
     
     CLASS = 258
     ELSE = 259
@@ -97,7 +108,33 @@ class Lexer:
 
     def lex(self, input_stream):
         print("inside the method 'lex' in Lexer class")
+        input_ptr = 0
+        while input_ptr < len(input_stream):
+            match = None
+            for rule in self.rules[self.curr_state ]:
+                regex = re.compile(rule.reg_expr)
+                match = regex.match(input_stream, input_ptr)
+                if match:
+                    if rule.tok_type and (rule.tok_type is not TokType.WHITESPACE and rule.tok_type is not TokType.THROWAWAY):
+                        self.tokenize(rule.tok_type, match.group(0))
+                        if rule.tok_type == TokType.NEWLINE:
+                            self.line_number += 1
+      
+                        if (self.debug and rule.tok_type != TokType.NEWLINE):
+                            #TODO: Use rule.tok_type.name once all tokens are enumerated
+                            print('{}{}'.format(self.line_number, ' ' + str(self.curr_state) + ' ' + str(rule.tok_type.name) + ' ' + '"' + match.group(0)) + '"')
 
+                    if rule.state:
+                        self.begin(rule.state)
+                    break
+
+            if not match:
+                sys.stderr.write('Illegal character: %s\\n' % input_stream[input_ptr])
+                sys.exit(1)
+            else:
+                input_ptr = match.end(0)
+
+        return self.tokens
 
 
 
@@ -107,7 +144,6 @@ if __name__ == '__main__':
     with open(sys.argv[1], 'r') as input_file:
         data = input_file.read()
     
-    debug = sys.argv[2:]
     print(data)
 
     lexer = Lexer(True)
